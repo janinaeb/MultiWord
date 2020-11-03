a65ca2f8c21f6f71e57c5a6585c26bf714c09c76
Janina Eriksson Bergström <janina.bergstrom@consid.se>
2020-11-03 20:09:26 +0100

    refactored read file and calculating matches

---
+++ MultiWord/FileHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace MultiWord
+{
+    public static class FileHandler
+    {
+        public static List<string> ReadFile(string name)
+        {
+            string line;
+            List<string> lines = new List<string>();
+
+            // Read the file and display it line by line.  
+            StreamReader file = new StreamReader(name);
+            while ((line = file.ReadLine()) != null)
+            {
+                lines.Add(line);
+            }
+
+            file.Close();
+
+            return lines;
+        }
+    }
+}
--- MultiWord/Program.cs
+++ MultiWord/Program.cs
@@ -13,20 +13,38 @@ namespace MultiWord
             string filePath = @"Files\";
 
             // Get all letters from the text to search
-            List<string> text = ReadFile($"{filePath}Hamlet.txt");
-            Dictionary<char, int> textLetters = string.Join("", text).GetLetterCount();
+            List<string> text = FileHandler.ReadFile($"{filePath}Hamlet.txt");
+            Dictionary<char, int> allTextLetters = string.Join("", text).GetLetterCount();
 
             // Get the dictionary of words to match on
-            List<string> dictionary = ReadFile($"{filePath}Ordlista.txt");
+            List<string> dictionary = FileHandler.ReadFile($"{filePath}Ordlista.txt");
 
             // Find all possible matching words
             IEnumerable<string> allMatchingWords = dictionary
-                .Where(word => word.GetLetterCount().ExistsIn(textLetters));
+                .Where(word => word.GetLetterCount().ExistsIn(allTextLetters));
 
             Console.WriteLine($"Found at total {allMatchingWords.Count()} matching words.");
 
             // Get the most optimal matching setup
             List<string> bestMatchingWords = new List<string>();
+            List<string> currentMatchingWords = CalculateMatches(allMatchingWords, allTextLetters);
+
+            // TODO: Find the most optimal matchings!
+            bestMatchingWords = currentMatchingWords;
+
+            File.WriteAllLines($"{filePath}Output-Örebro-Karlstad.txt", bestMatchingWords);
+
+            // Write output messages
+            Console.WriteLine($"Found most optimally {bestMatchingWords.Count()} matching words.");
+            Console.WriteLine("Opening the file directory for you now...");
+
+            Process.Start("explorer.exe", filePath);
+            
+            Console.WriteLine("Bye!");
+        }
+
+        private static List<string> CalculateMatches(IEnumerable<string> allMatchingWords, Dictionary<char, int> textLetters)
+        {
             List<string> currentMatchingWords = new List<string>();
             Dictionary<char, int> textLettersLeft = new Dictionary<char, int>(textLetters);
             foreach (var matchingWord in allMatchingWords)
@@ -52,35 +70,7 @@ namespace MultiWord
                     }
                 }
             }
-
-            // TODO: Find the most optimal matchings!
-            bestMatchingWords = currentMatchingWords;
-
-            File.WriteAllLines($"{filePath}Output-Örebro-Karlstad.txt", bestMatchingWords);
-
-
-            Console.WriteLine($"Found most optimally {bestMatchingWords.Count()} matching words.");
-            Console.WriteLine("Opening the file directory for you now...");
-            Process.Start("explorer.exe", filePath);
-            Console.WriteLine("Bye!");
-            Console.ReadLine();
-        }
-
-        private static List<string> ReadFile(string name)
-        {
-            string line;
-            List<string> lines = new List<string>();
-
-            // Read the file and display it line by line.  
-            StreamReader file = new StreamReader(name);
-            while ((line = file.ReadLine()) != null)
-            {
-                lines.Add(line);
-            }
-
-            file.Close();
-
-            return lines;
+            return currentMatchingWords;
         }
     }
 }
