5ff52c547902e092e586643e67d7a85f4bc95ec7
Janina Eriksson Bergström <janina.bergstrom@consid.se>
2020-11-03 18:31:51 +0100

    match by letters and letter count

---
+++ MultiWord/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MultiWord
+{
+    public static class Extensions
+    {
+        public static Dictionary<char, int> GetLetterCount(this string word)
+        {
+            return word
+                .ToUpperInvariant()
+                .ToCharArray()
+                .GroupBy(letter => letter)
+                .ToDictionary(
+                    key => key.Key,
+                    value => value.Count()
+            );
+        }
+    }
+}
---
+++ MultiWord/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MultiWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> text = ReadFile(@"Files\Hamlet.txt");
+            Dictionary<char, int> textLetters = string.Join("", text).GetLetterCount();
+
+            List<string> dictionary = ReadFile(@"Files\Ordlista.txt");
+            IEnumerable<string> matchingWords = dictionary
+                .Where(word => word
+                    .GetLetterCount()
+                    .All(letter => textLetters.TryGetValue(letter.Key, out int textLetterCount) && textLetterCount >= letter.Value));
+
+            File.WriteAllLines(@"Files\Output-Örebro-Karlstad.txt", matchingWords);
+
+            foreach (var word in matchingWords)
+            {
+                Console.WriteLine(word);
+            }
+            Console.ReadLine();
+        }
+
+        private static List<string> ReadFile(string name)
+        {
+            string line;
+            List<string> lines = new List<string>();
+
+            // Read the file and display it line by line.  
+            StreamReader file = new StreamReader(name);
+            while ((line = file.ReadLine()) != null)
+            {
+                lines.Add(line);
+            }
+
+            file.Close();
+
+            return lines;
+        }
+    }
+}
